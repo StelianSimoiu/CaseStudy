8829f13d373e3763b0c09735782e7246030601ba
StelianSimoiu <stelian.simoiu@outlook.com>
2015-05-21 15:45:59 +0100

    Rename

--- ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CaseStudy")]
+[assembly: AssemblyTitle("CaseStudyConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CaseStudy")]
+[assembly: AssemblyProduct("CaseStudyConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
